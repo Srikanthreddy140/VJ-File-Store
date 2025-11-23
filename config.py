4479f206b92ee4507f4f59b65a2e20111808c988
Srikanthreddy140 <147992962+Srikanthreddy140@users.noreply.github.com>
2025-11-23 22:42:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://jojaxi3214_db_user:Hgb
 CDB_NAME = environ.get("CDB_NAME", "ROYALBOMMA")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://jojaxi3214_db_user:Hgbkidccy7nCA28m@cluster0.mviuwum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://srikanthreddycmd77:BZd72iOaOfcXrvlP@cluster0.hquo2bt.mongodb.net/?appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "ROYALBOMMA")
 
 # Auto Delete Information
